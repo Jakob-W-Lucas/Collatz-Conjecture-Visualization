7c679326839427b6779b1e666b56c8b1184ab603
t0astie <jakob.w.lucas@gmail.com>
2025-02-01 12:34:00 +1100

    Create axis multiplier and documentation

--- collatz.py
+++ collatz.py
@@ -2,31 +2,45 @@ import pygame
 
 pygame.init()
 
+# Screen size and creation
 SCREEN_WIDTH = 2560
 SCREEN_HEIGHT = 1440
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+# Size of each square to display
 square_size = 10
+# Distance between squares on each axis
+axis_multiplier = [2, 2]
 
+# Gets the next value of a number based on the Collatx conjecture 
 def get_next(num: int) -> int:
     return num // 2 if num % 2 == 0 else 3 * num + 1
 
+# Creates the pygame rect squares for each number in a sequence
 def create_num_squares(num: int) -> list[pygame.Rect]:
     m = num
     step = 0
     
     squares = []
     
-    squares.append(pygame.Rect((step, m * square_size, square_size, square_size)))
+    # Initial square in a sequence 
+    squares.append(pygame.Rect((step, m * square_size * axis_multiplier[1], square_size, square_size)))
+    
+    # Until the number gets to 1, continue to create the sqaures 
     while m != 1:
+        # Get the next value in the sequence
         m = get_next(m)
-        step += square_size
-        squares.append(pygame.Rect((step, m * square_size, square_size, square_size)))
+        step += square_size * axis_multiplier[0]
+        # Create the new square
+        squares.append(pygame.Rect((step, m * square_size * axis_multiplier[1], square_size, square_size)))
     
     return squares
 
-group_squares = []
+
+
+# List of squares for every sequence
+sequence_squares = []
 
 counter = 0
 n = 0
@@ -34,33 +48,42 @@ n = 0
 run = True
 pressed = False
 
+# Game loop
 while run:
     
+    # Create the screen and get the key presses
     screen.fill((255, 255, 255))
-    
     key = pygame.key.get_pressed()
-    if key[pygame.K_ESCAPE]:
-       run = False
-        
+    
+    # Returns true once when the key is initially pressed
     if key[pygame.K_j] and pressed != True:
         counter += 1
         pressed = True
-        
+    
+    # Only allow to increment when key is let go
     if not key[pygame.K_j]:
         pressed = False
     
+    # Get the new list of squares for the next sequence
     if n != counter:
         n = counter
-        group_squares.append(create_num_squares(counter))
-            
-    for group in group_squares:
+        sequence_squares.append(create_num_squares(counter))
+    
+    # Draw each square from every sequence
+    for group in sequence_squares:
         for r in group:
             pygame.draw.rect(screen, (0, 0, 0), r)
-        
+    
+    # Exit
+    if key[pygame.K_ESCAPE]:
+       run = False
+       
+    # Quit game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     
+    # Update the game display
     pygame.display.update()
 
 pygame.quit()
