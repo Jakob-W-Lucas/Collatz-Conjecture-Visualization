a71beca9c668a79c92441809c754f3a81dab8d34
t0astie <jakob.w.lucas@gmail.com>
2025-02-01 18:03:19 +1100

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -6,6 +6,7 @@ Author: Jakob Lucas
 
 import pygame
 import math
+import random
 
 pygame.init()
 
@@ -17,8 +18,12 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # Distance between squares on each axis
 start_degree = 90
+# Change in degree per step in a sequence
 degree_increment = 10
-line_length = 30
+# Standard length of a line
+line_length = 25
+# Degree that a line turns when a number is odd
+line_degree_turn = 45
 
 # Starting point for squares
 square_spawn = [0, SCREEN_HEIGHT]
@@ -27,7 +32,7 @@ square_spawn = [0, SCREEN_HEIGHT]
 def get_next(num: int) -> int:
     return num // 2 if num % 2 == 0 else 3 * num + 1
     
-def get_sequence_positions(n: int) -> list[tuple[int, int]]:
+def get_sequence_positions(n: int, s_degree: float) -> list[tuple[int, int]]:
     m = n
     step = 1
     
@@ -39,14 +44,14 @@ def get_sequence_positions(n: int) -> list[tuple[int, int]]:
     
     positions = [None] * len(sequence)
     
-    x = math.cos(math.radians(start_degree)) * line_length
-    y = math.sin(math.radians(start_degree)) * line_length
+    x = math.cos(math.radians(s_degree)) * line_length
+    y = math.sin(math.radians(s_degree)) * line_length
     
     # Until the number gets to 1, continue to create the squares 
     for num in sequence:
         
-        angle = start_degree + step * degree_increment + (90 * (num % 2))
-        length = max(line_length // 5, line_length - step)
+        angle = s_degree + step * degree_increment + (line_degree_turn * (num % 2))
+        length = max(line_length // 5, line_length - step) * math.log2(num)
         
         x = math.cos(math.radians(angle)) * length + x
         y = math.sin(math.radians(angle)) * length + y
@@ -63,7 +68,7 @@ def get_sequence_positions(n: int) -> list[tuple[int, int]]:
 # List of squares for every sequence
 sequence_positions = []
 
-counter = 888
+counter = 2
 n = 0
 
 run = True
@@ -88,13 +93,14 @@ while run:
     # Get the new list of squares for the next sequence
     if n != counter:
         n = counter
-        sequence_positions.append(get_sequence_positions(counter))
+        sequence_positions.append(get_sequence_positions(counter, start_degree))
+        start_degree += random.randint(0, 45)
     
     # Draw each square from every sequence
     for g in range(len(sequence_positions)):
         for s in range(1, len(sequence_positions[g])):
             pygame.draw.line(screen, pygame.Color(213, 224, 216, 0), sequence_positions[g][s - 1], sequence_positions[g][s])
-            pygame.draw.rect(screen, pygame.Color(255, 0, 0), pygame.Rect(sequence_positions[g][s - 1][0], sequence_positions[g][s - 1][1], 3, 3), width=0)
+        pygame.draw.rect(screen, pygame.Color(255, 0, 0), pygame.Rect(sequence_positions[g][s][0], sequence_positions[g][s][1], 3, 3), width=0)
     
     # Exit
     if key[pygame.K_ESCAPE]:
