8ca097b00c545e81755e929f9f0da341dfb693f7
t0astie <jakob.w.lucas@gmail.com>
2025-02-01 14:23:23 +1100

    Create lines to replace squares and fix colour

--- collatz.py
+++ collatz.py
@@ -26,37 +26,34 @@ square_spawn = [0, SCREEN_HEIGHT]
 
 # Gets the next value of a number based on the Collatx conjecture 
 def get_next(num: int) -> int:
+    if num == 1: return 0
     return num // 2 if num % 2 == 0 else 3 * num + 1
 
 # Creates the pygame rect squares for each number in a sequence
-def create_num_squares(num: int) -> list[pygame.Rect]:
+def get_sequence_positions(num: int) -> list[(int, int)]:
     m = num
     step = 0
     
-    squares = []
-    
-    # Initial square in a sequence 
-    squares.append(pygame.Rect((step, square_spawn[1] - (m * square_size * axis_multiplier[1]), square_size, square_size)))
+    positions = []
     
     # Until the number gets to 1, continue to create the sqaures 
-    while m != 1:
-    
-        # Get the next value in the sequence
-        m = get_next(m)
-        step += square_size * axis_multiplier[0]
+    while m >= 1:
         
         x = step 
         y = square_spawn[1] - (m * square_size * axis_multiplier[1])
         
-        # Create the new square
-        squares.append(pygame.Rect((x, y, square_size, square_size)))
+        positions.append((x, y))
+        
+        # Get the next value in the sequence
+        m = get_next(m)
+        step += square_size * axis_multiplier[0]
     
-    return squares
+    return positions
 
 
 
 # List of squares for every sequence
-sequence_squares = []
+sequence_positions = []
 
 counter = 0
 n = 0
@@ -68,7 +65,7 @@ pressed = False
 while run:
     
     # Create the screen and get the key presses
-    screen.fill((255, 255, 255))
+    screen.fill((24, 14, 26))
     key = pygame.key.get_pressed()
     
     # Returns true once when the key is initially pressed
@@ -83,12 +80,12 @@ while run:
     # Get the new list of squares for the next sequence
     if n != counter:
         n = counter
-        sequence_squares.append(create_num_squares(counter))
+        sequence_positions.append(get_sequence_positions(counter))
     
     # Draw each square from every sequence
-    for group in sequence_squares:
-        for r in group:
-            pygame.draw.rect(screen, (0, 0, 0), r)
+    for g in range(len(sequence_positions)):
+        for s in range(1, len(sequence_positions[g])):
+            pygame.draw.line(screen, pygame.Color(213, 224, 216, 0), sequence_positions[g][s - 1], sequence_positions[g][s])
     
     # Exit
     if key[pygame.K_ESCAPE]:
